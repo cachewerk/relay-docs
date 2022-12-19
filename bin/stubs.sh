93af1e0a9ccf4ebcbba2ddd7d4af098ecb590d27
Till Krüss <till@kruss.io>
2022-12-19 10:53:47 -0800

    fix var

--- bin/stubs.sh
+++ bin/stubs.sh
@@ -9,5 +9,5 @@ wget -q -P stubs/ "https://cachewerk.s3.amazonaws.com/relay/LATEST"
 wget -q -P stubs/develop "https://cachewerk.s3.amazonaws.com/relay/dev/relay.stub.php"
 
 LATEST=$(cat stubs/LATEST)
-BASE_VERSION=${VERSION:1:2}x
+BASE_VERSION=${LATEST:1:2}x
 wget -q -P "stubs/${BASE_VERSION}" "https://cachewerk.s3.amazonaws.com/relay/${LATEST}/relay.stub.php"
