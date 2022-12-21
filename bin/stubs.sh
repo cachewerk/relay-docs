0b7d5cd1c3ed2e34b1971db10fff20caaf3418db
Till Krüss <till@kruss.io>
2022-12-21 09:46:02 -0800

    use new latest file

--- bin/stubs.sh
+++ bin/stubs.sh
@@ -8,6 +8,6 @@ wget -q -P stubs/ "https://builds.r2.relay.so/meta/latest"
 
 wget -q -P stubs/develop "https://builds.r2.relay.so/dev/relay.stub.php"
 
-LATEST=$(cat stubs/LATEST)
-BASE_VERSION=${LATEST:1:2}x
-wget -q -P "stubs/${BASE_VERSION}" "https://builds.r2.relay.so/${LATEST}/relay.stub.php"
+VERSION_LATEST=$(cat stubs/latest)
+VERSION_BASE=${VERSION_LATEST:1:2}x
+wget -q -P "stubs/${VERSION_BASE}" "https://builds.r2.relay.so/${VERSION_LATEST}/relay.stub.php"
