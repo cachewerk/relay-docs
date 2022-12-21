76eb98a1696d4feeafbeec2530b17e805de7a484
Till Krüss <till@kruss.io>
2022-12-21 09:53:16 -0800

    tweaks

--- bin/stubs.sh
+++ bin/stubs.sh
@@ -2,12 +2,13 @@
 
 set -e
 
-rm -rf stubs
+mkdir -p stubs
+rm -rf stubs/*
 
-wget -q -P stubs/ "https://builds.r2.relay.so/meta/latest"
+wget -q -O stubs/LATEST "https://builds.r2.relay.so/meta/latest"
 
 wget -q -P stubs/develop "https://builds.r2.relay.so/dev/relay.stub.php"
 
-VERSION_LATEST=$(cat stubs/latest)
+VERSION_LATEST=$(cat stubs/LATEST)
 VERSION_BASE=${VERSION_LATEST:1:2}x
 wget -q -P "stubs/${VERSION_BASE}" "https://builds.r2.relay.so/${VERSION_LATEST}/relay.stub.php"
