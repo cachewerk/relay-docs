340ad5e570c2083e3ee0188fc24b3976ea773a35
Till Krüss <till@kruss.io>
2023-01-13 08:51:52 -0800

    add placeholder script

--- bin/stubs-php.sh
+++ bin/stubs-php.sh
@@ -5,21 +5,17 @@ set -e
 mkdir -p stubs
 rm -rf stubs/*
 
-LATEST_VERSION=$(curl -sSL https://builds.r2.relay.so/meta/latest)
+LATEST_VERSION=dev
+# LATEST_VERSION=$(curl -sSL https://builds.r2.relay.so/meta/latest)
 
 wget -q -P stubs "https://builds.r2.relay.so/${LATEST_VERSION}/relay.stub.php"
 
-# PHP 8.2 stubs
-mv stubs/relay.stub.php stubs/relay-8.2.php
+## Append curly brackets to functions
+gsed -i -r --posix 's/);$/) { }/' stubs/relay.stub.php
+gsed -i -r --posix 's/):(.*);$/):\1 { }/' stubs/relay.stub.php
 
-# Append curly brackets to functions
-gsed -i --posix -r 's/):(.*);/):\1 { }/' stubs/relay-8.2.php
+## Inject `LanguageLevelTypeAware` attributes
+# TODO: ...
 
-# PHP 8.1 stubs
-cp stubs/relay-8.2.php stubs/relay-8.1.php
-
-# PHP 8.0 stubs
-cp stubs/relay-8.1.php stubs/relay-8.0.php
-
-# PHP 7.4 stubs
-cp stubs/relay-8.0.php stubs/relay-7.4.php
+# docker-compose -f docker-compose.yml run test_runner composer install --ignore-platform-reqs
+# docker-compose -f docker-compose.yml run test_runner composer cs-fix
